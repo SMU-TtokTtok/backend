581342dfe85126b21992633383ef1b03754115c9
codemaker-kim <tnals72441@daum.net>
2025-06-21 05:39:20 +0900

    [Feat]: 액세스 토큰 유효 기간 변경
    1시간 -> 15분으로 감소

--- src/main/java/org/project/ttokttok/global/jwt/TokenExpiry.java
+++ src/main/java/org/project/ttokttok/global/jwt/TokenExpiry.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum TokenExpiry {
-    ACCESS_TOKEN_EXPIRY_TIME(60 * 60 * 1000L), // 1시간
+    ACCESS_TOKEN_EXPIRY_TIME(15 * 60 * 1000L), // 15분
     REFRESH_TOKEN_EXPIRY_TIME(7 * 24 * 60 * 60 * 1000L); // 7일
 
     final Long expiry;
