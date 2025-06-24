ca3ce11fb984ad618070a00c58b92de7bf584bb5
codemaker-kim <tnals72441@daum.net>
2025-06-24 21:25:39 +0900

    [#3] - 잘못된 클래스 상속받던 예외 수정

--- src/main/java/org/project/ttokttok/global/jwt/exception/RefreshTokenExpiredException.java
+++ src/main/java/org/project/ttokttok/global/jwt/exception/RefreshTokenExpiredException.java
@@ -1,8 +1,9 @@
 package org.project.ttokttok.global.jwt.exception;
 
 import org.project.ttokttok.global.error.ErrorMessage;
+import org.project.ttokttok.global.error.exception.CustomException;
 
-public class RefreshTokenExpiredException extends RuntimeException {
+public class RefreshTokenExpiredException extends CustomException {
     public RefreshTokenExpiredException() {
         super(ErrorMessage.REFRESH_TOKEN_EXPIRED);
     }
