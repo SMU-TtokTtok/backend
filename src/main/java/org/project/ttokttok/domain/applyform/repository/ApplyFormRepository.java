411a85f7ef7d675e7ea7d987f06d3fb0c9c12427
codemaker-kim <tnals72441@daum.net>
2025-07-04 18:57:11 +0900

    [#8] - 지원 폼 리포지토리 추가

---
+++ src/main/java/org/project/ttokttok/domain/applyform/repository/ApplyFormRepository.java
@@ -0,0 +1,7 @@
+package org.project.ttokttok.domain.applyform.repository;
+
+import org.project.ttokttok.domain.applyform.domain.ApplyForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplyFormRepository extends JpaRepository<ApplyForm, String>{
+}
