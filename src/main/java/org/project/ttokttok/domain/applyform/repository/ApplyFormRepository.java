f9c7e301d9b5c258efcc20644a4f3770f3cc0632
codemaker-kim <tnals72441@daum.net>
2025-07-13 03:46:40 +0900

    [#29] - 메서드 미존재로 발생하던 컴파일 에러 수정

--- src/main/java/org/project/ttokttok/domain/applyform/repository/ApplyFormRepository.java
+++ src/main/java/org/project/ttokttok/domain/applyform/repository/ApplyFormRepository.java
@@ -1,6 +1,7 @@
 package org.project.ttokttok.domain.applyform.repository;
 
 import org.project.ttokttok.domain.applyform.domain.ApplyForm;
+import org.project.ttokttok.domain.applyform.domain.enums.ApplyFormStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface ApplyFormRepository extends JpaRepository<ApplyForm, String> {
     Optional<ApplyForm> findTopByClubIdOrderByCreatedAtDesc(String clubId);
 
     List<ApplyForm> findByClubId(String clubId);
+
+    Optional<ApplyForm> findByClubIdAndStatus(String clubId, ApplyFormStatus status);
 }
