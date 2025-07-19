5950f21728877cf971da69075739723303db1a65
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-07-20 05:09:05 +0900

    [#62] - Swagger Config 수정 (임시방편)

--- src/main/java/org/project/ttokttok/global/config/SwaggerConfig.java
+++ src/main/java/org/project/ttokttok/global/config/SwaggerConfig.java
@@ -5,10 +5,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server; // Server 클래스 임포트
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Collections;
+import java.util.List; // List 임포트
 
 @Configuration
 public class SwaggerConfig {
@@ -25,10 +27,17 @@ public class SwaggerConfig {
         // 보안 요구사항 설정
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("cookieAuth");
 
+        // --- 이 부분 추가/수정 ---
+        // 서버 URL을 명시적으로 설정 (하드코딩)
+        // application.yml 설정이 적용되지 않는 문제를 우회하기 위함
+        Server server = new Server().url("https://www.hearmeout.kr").description("Production Server");
+        // --- 여기까지 ---
+
         return new OpenAPI()
                 .info(info)
                 .components(new Components().addSecuritySchemes("cookieAuth", apiKey))
-                .security(Collections.singletonList(securityRequirement));
+                .security(Collections.singletonList(securityRequirement))
+                .servers(List.of(server)); // 서버 설정 추가
     }
 
     // todo: 추후 수정
