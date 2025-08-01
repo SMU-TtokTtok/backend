574abe0a5c515935b0af52e81cb1e98f80faeac7
codemaker-kim <tnals72441@daum.net>
2025-08-01 12:27:11 +0900

    [#90] - 상수 값 추가

--- src/main/java/org/project/ttokttok/domain/applicant/controller/enums/Kind.java
+++ src/main/java/org/project/ttokttok/domain/applicant/controller/enums/Kind.java
@@ -1,6 +1,14 @@
 package org.project.ttokttok.domain.applicant.controller.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Kind {
-    DOCUMENT,
-    INTERVIEW
+    
+    DOCUMENT("DOCUMENT"),
+    INTERVIEW("INTERVIEW");
+
+    final String value;
 }
