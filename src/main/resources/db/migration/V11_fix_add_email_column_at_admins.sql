babb25178250275c305bb2190b7de52f16900c99
codemaker-kim <tnals72441@daum.net>
2026-02-08 15:55:15 +0900

    [#285] - 마이그레이션 쿼리 수정

---
+++ src/main/resources/db/migration/V11_fix_add_email_column_at_admins.sql
@@ -0,0 +1,17 @@
+-- admin 테이블에 email 컬럼 추가
+-- 1. 먼저 NULL 허용으로 컬럼 추가
+ALTER TABLE admins
+    ADD COLUMN email VARCHAR(255);
+
+-- 2. 기존 데이터에 고유한 임시 이메일 설정
+UPDATE admins
+SET email = CONCAT('admin_', id, '@temp.com')
+WHERE email IS NULL;
+
+-- 3. NOT NULL 제약조건 추가
+ALTER TABLE admins
+    MODIFY COLUMN email VARCHAR(255) NOT NULL;
+
+-- 4. UNIQUE 제약조건 추가
+ALTER TABLE admins
+    ADD CONSTRAINT uk_admins_email UNIQUE (email);
